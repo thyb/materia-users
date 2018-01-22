bdfcebbc7c0e635425b2cbb1b3fda93a90f1615c
Thibaud Arnault <thibaud.arnault@gmail.com>
2018-01-21 20:23:57 -0400

    fix view

--- client/components/user-management-view/user-management-view.component.ts
+++ client/components/user-management-view/user-management-view.component.ts
@@ -2,32 +2,25 @@ const ngcore = (window as any).angular.core;
 const Component = ngcore.Component;
 
 @Component({
-  selector: "materia-user-management-view",
-  templateUrl: "./user-management-view.component.html",
-  styleUrls: ["./user-management-view.component.scss"]
+	selector: "materia-user-management-view",
+	templateUrl: "./user-management-view.component.html",
+	styleUrls: ["./user-management-view.component.scss"]
 })
-
 export class UserManagementViewComponent {
-  userSelected: any;
-  users: any;
-  constructor(private app: any) {
-    this.app.entities.get('user').getQuery('list').run().then(users => {
-      this.users = users.rows
-    }).catch(e => {
-      console.log('error', e, e.stack)
-    })
-  }
+	userSelected: any;
+	users: any;
+	constructor() {}
 
-  selectUser(user) {
-    this.userSelected = user
-  }
+	selectUser(user) {
+		this.userSelected = user;
+	}
 
-  signup(ev) {
-    /* QueryService.execute(
+	signup(ev) {
+		/* QueryService.execute(
       $rootScope.app.entities
         .get('user')
         .getQuery('signup')
       , null, ev
     )*/
-  }
+	}
 }

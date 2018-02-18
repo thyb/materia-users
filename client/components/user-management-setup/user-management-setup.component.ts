f06e14127418d729f28f2b6d2d7af8692e88c638
GeoAstronaute <jean.marc.royy@gmail.com>
2018-02-18 01:46:28 +0100

    (SetupComponent) First integration

--- client/components/user-management-setup/user-management-setup.component.ts
+++ client/components/user-management-setup/user-management-setup.component.ts
@@ -1,35 +1,53 @@
 const ngcore = (window as any).angular.core;
+const ngforms = (window as any).angular.forms
 const Component = ngcore.Component;
 const EventEmitter = ngcore.EventEmitter;
 const OnInit = ngcore.OnInit;
 const Output = ngcore.Output;
+import { FormGroup } from "@angular/forms";
+import { Input } from "@angular/core";
+
 
 export interface UserManagementConfig {
-	fields: any[],
-	salt: string,
-	type: string,
-	// emails: {}
+    fields: any[],
+    salt: string,
+    type: string,
+    // emails: {}
 }
 
 @Component({
-	selector: "materia-user-management-setup",
-	templateUrl: "./user-management-setup.component.html",
-	styleUrls: ["./user-management-setup.component.scss"]
+    selector: "materia-user-management-setup",
+    templateUrl: "./user-management-setup.component.html",
+    styleUrls: ["./user-management-setup.component.scss"]
 })
 export class UserManagementSetupComponent {
-	addon: any;
+    addon: any;
+    loginForm: FormGroup;
+    emailForm: FormGroup;
+    userForm: FormGroup;
+    @Input() formBuilder;
 
-	config: any;
+    config: any;
 
-	@Output() save = new EventEmitter();
-	@Output() cancel = new EventEmitter();
+    @Output() save = new EventEmitter();
+    @Output() cancel = new EventEmitter();
 
-	constructor() {}
+    constructor() {
+        this.loginForm = this.formBuilder.group({
+            test: [""]
+        });
+        this.emailForm = this.formBuilder.group({
+            test: [""]
+        });
+        this.userForm = this.formBuilder.group({
+            test: [""]
+        });
+    }
 
-	saveClick() {
-		this.save.emit(this.config);
-	}
-	cancelClick() {
-		this.cancel.emit();
-	}
+    saveClick() {
+        this.save.emit(this.config);
+    }
+    cancelClick() {
+        this.cancel.emit();
+    }
 }

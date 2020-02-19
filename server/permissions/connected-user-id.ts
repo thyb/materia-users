55ff30077678650a5c2ec1d759eb49a8d3dbee0f
Thibaud Arnault <thyb@getmateria.com>
2020-02-19 01:19:55 +0100

    fix(permission): ConnectedUserId was not working with token method

--- server/permissions/connected-user-id.ts
+++ server/permissions/connected-user-id.ts
@@ -3,17 +3,36 @@ export function defineConnectedUserIdPermission(app, config) {
     name: 'Connected User ID',
     description: 'Inject the id of the connected user',
     middleware: (req, res, next) => {
-      if (req.user) {
-        if (req.method === 'get' || req.method === 'delete') {
-          req.query.id_user = req.user.id_user;
-        } else {
-          req.body.id_user = req.user.id_user;
-        }
-        return next();
+      if (config.method === 'token') {
+        return app.server.passport.authenticate('usersAccessToken', {
+          session: false
+        })(req, res, err => {
+          if (! err) {
+            if (req.method === 'get' || req.method === 'delete') {
+              req.query.id_user = req.user.id_user;
+            } else {
+              req.body.id_user = req.user.id_user;
+            }
+            return next()
+          } else {
+            const e: any = new Error('Unauthorized');
+            e.statusCode = 401;
+            throw e;
+          }
+        });
       } else {
-        const e: any = new Error('Unauthorized');
-        e.statusCode = 401;
-        throw e;
+        if (req.user) {
+          if (req.method === 'get' || req.method === 'delete') {
+            req.query.id_user = req.user.id_user;
+          } else {
+            req.body.id_user = req.user.id_user;
+          }
+          return next();
+        } else {
+          const e: any = new Error('Unauthorized');
+          e.statusCode = 401;
+          throw e;
+        }
       }
     },
     readOnly: true,
